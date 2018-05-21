5a4656b9a291b5ae92ddc3d07836182637ec2ebe
Enji <enjikim17@gmail.com>
2018-05-20 22:04:32 -0700

    stringifies 'string' and 'number' values in object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,20 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var json = "{";
+
+  for (var key in obj) {
+
+    if (typeof obj[key] === 'string') {
+      json = json.concat("\"" + key + "\"" + ":" + "\"" + obj[key] + "\"");
+    } else if (typeof obj[key] === 'number') {
+      json = json.concat("\"" + key + "\"" + ":" + obj[key]);
+    }
+
+    json = json.concat(",");
+  }
+  
+  json = json.slice(0, json.length - 1);
+  return json.concat("}");
+
 };
